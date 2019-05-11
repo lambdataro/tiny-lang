39eb75aa95c09a8372f9695d0e322aa4acf8f7ed
lambdataro <lambda.taro@gmail.com>
2019-05-11 12:26:08 +0900

    不要なファイルを削除

--- Main.c
+++ Main.c
@@ -1,80 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include "Stream.h"
-#include "Util.h"
-#include "Lexing.h"
-#include "Parsing.h"
-#include "Eval.h"
 
-static void runFile(const char *filename);
-static void runRepl(void);
-static void parseAndRunCode(Stream *stream, SymbolTable *table);
-
-int main(int argc, char *argv[])
+int main(void)
 {
-    if (argc == 2) {
-        runFile(argv[1]);
-        return EXIT_SUCCESS;
-    }
-
-    if (argc == 1) {
-        printf("TinyLang\n");
-        runRepl();
-        return EXIT_SUCCESS;
-    }
-
-    fprintf(stderr, "Usage: TinyLang [filename]\n");
-    return EXIT_FAILURE;
-}
-
-static void runFile(const char *filename)
-{
-    Stream *stream = createFileStream(filename);
-    if (!stream) {
-        fprintf(stderr, "failed to open file: \"%s\"\n", filename);
-        exit(EXIT_FAILURE);
-    }
-    SymbolTable *table = createSymbolTable();
-    parseAndRunCode(stream, table);
-    destroyStream(stream);
-    destroySymbolTable(table);
-}
-
-static void runRepl(void)
-{
-    SymbolTable *table = createSymbolTable();
-    while (true) {
-        printf("\n");
-        statistics(table->pool);
-        printf("> ");
-        fflush(stdout);
-        char *str = readConsoleUntilEnterTwice();
-        if (!str) {
-            free(str);
-            break;
-        }
-        Stream *stream = createStringStream(str);
-        free(str);
-        parseAndRunCode(stream, table);
-        destroyStream(stream);
-    }
-    destroySymbolTable(table);
-}
-
-static void parseAndRunCode(Stream *stream, SymbolTable *table)
-{
-    LexingState *state = createLexingState(stream);
-    Ast *ast = startParse(state);
-    fprintAst(stdout, ast, 0);
-    fprintf(stdout, "\n\n");
-
-    if (ast->type != AST_ERROR) {
-        Value *value = startEval(table, ast);
-        destroyValue(table->pool, value);
-    }
-
-    destroyAst(ast);
-    destroyLexingState(state);
+    printf("TinyLang\n");
+    return EXIT_SUCCESS;
 }
