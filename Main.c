92fe3a981410cc6241e148f13cea7647d06813eb
lambdataro <lambda.taro@gmail.com>
2019-05-11 13:17:43 +0900

    hello, world. を出力

--- Main.c
+++ Main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void outputHelloWorld(void);
+
 int main(void)
 {
     printf("TinyLang\n");
+    fflush(stdout);
+
+    outputHelloWorld();
+    system("gcc -o prog build.c");
+    system("prog");
+
     return EXIT_SUCCESS;
+}
+
+void outputHelloWorld(void)
+{
+    FILE *file = fopen("build.c", "w");
+    if (!file) {
+        fprintf(stderr, "failed to create output file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(file,
+        "#include <stdio.h>\n"
+        "int main(void) {\n"
+        "  printf(\"hello, world.\\n\");\n"
+        "  return 0;\n"
+        "}\n");
+
+    fclose(file);
 }
