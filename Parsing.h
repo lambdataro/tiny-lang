60c1f87d89a904059de2c07bd6e21ba574ee9443
lambdataro <lambda.taro@gmail.com>
2019-05-19 03:07:04 +0900

    不要なファイルを削除

--- Parsing.h
+++
@@ -1,9 +0,0 @@
-#ifndef TINY_LANG_PARSING_H_INCLUDED
-#define TINY_LANG_PARSING_H_INCLUDED
-
-#include "Lexing.h"
-#include "Ast.h"
-
-Ast *startParse(LexingState *state);
-
-#endif // TINY_LANG_PARSING_H_INCLUDED
